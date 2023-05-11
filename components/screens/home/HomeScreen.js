bc07d02739aa9a48a818b767a0c9da8363ca3378
Pranav Puranam <pranav.puranam@gmail.com>
2023-05-10 17:12:12 -0700

    completed the navbar component

--- components/screens/home/HomeScreen.js
+++ components/screens/home/HomeScreen.js
@@ -3,7 +3,7 @@ import NavBar from '../../overlays/NavBar';
 
 export default function HomeScreen() {
     return (
-        <View className="flex-1 justify-center items-center">
+        <View className="flex-1 justify-center items-center bg-sky-950">
             <Text className="text-red-600">Hello</Text>
             <NavBar />
         </View>
