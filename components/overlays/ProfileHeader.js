c3236c6237ee715963226c0ad306779206154a6c
Bach Ngo <bachngo@s-169-232-106-153.resnet.ucla.edu>
2023-05-13 23:28:42 -0700

    profile modal with more options, fix date option glitch, field filling errors, and submission modal message

--- components/overlays/ProfileHeader.js
+++ components/overlays/ProfileHeader.js
@@ -1,28 +1,157 @@
-import { View, Text, Image } from "react-native";
+import { useState } from "react";
+import { View, Text, Image, TextInput, SafeAreaView } from "react-native";
+import Modal from "react-native-modal";
 import TouchableScale from "react-native-touchable-scale";
 import pfp from "../../assets/Default_pfp.svg.png"
+import Cancel from "../../assets/cancel.svg"
+import Check from "../../assets/check.svg"
 
 
 
 export default function ProfileHeader({ name, icon }) {
+
+    const [screenVisible, setScreenVisible] = useState(false)
+    const [userName, setUserName] = useState(name)
+    const [changedName, setChangedName] = useState(null);
+    const [phonenumber, setPhoneNumber] = useState(null);
+    const [emergencyContact, setEmergencyContact] = useState(null);
+
+    const styles = {
+        inputText: "text-white text-lg align-left mt-4 ml-3",
+        inputField: "p-2 w-12/12 m-2 justify-start border-b-2 border-sky-300",
+    }
+    
     return (
-        <View
-        className="flex-row w-full h-12 mt-12 items-center"
-        >
-            <TouchableScale
-            className="flex-row w-1/2 h-12 items-center"
-            activeScale={0.97}
+        <>
+            <Modal
+            isVisible={screenVisible}
+            onBackdropPress={() => setScreenVisible(false)}
+            onDismiss={() => setScreenVisible(false)}
+            animationIn="slideInLeft"
+            animationOut="slideOutLeft"
+            hideModalContentWhileAnimating={true}
+            backdropOpacity={0.5}
+            animationInTiming={500}
+            animationOutTiming={500}
+            propagateSwipe={true}
+            className="justify-start m-0"
             >
-                <Image 
-                source={pfp}
-                className="w-8 h-8 mr-4 ml-6"
-                />
-                <Text
-                className="text-white "
+                <SafeAreaView
+                className="w-screen h-screen bg-sky-950 rounded-xl flex-col items-center"
                 >
-                    {name}
-                </Text>
-            </TouchableScale>
-        </View>
+                    <Text
+                    className="text-white mt-6 text-4xl font-bold text-center"
+                    >
+                        Edit Your Profile
+                    </Text>
+
+                    <View
+                    className="w-11/12 mt-4"
+                    >
+                        <Text
+                        className={styles.inputText}
+                        >
+                            Name:
+                        </Text>
+                        <TextInput
+                        className={styles.inputField + " h-10"}
+                        placeholderTextColor="#0284BE"
+                        placeholder="Ex: John/Jane Doe..."
+                        onChangeText={setChangedName}
+                        />
+                    </View>
+
+                    <View
+                    className="w-11/12 mt-4"
+                    >
+                        <Text
+                        className={styles.inputText}
+                        >
+                            Phone Number:
+                        </Text>
+                        <TextInput
+                        className={styles.inputField + " h-10"}
+                        placeholderTextColor="#0284BE"
+                        placeholder="Ex: (XXX) XXX - XXXX"
+                        onChangeText={setPhoneNumber}
+                        />
+                    </View>
+
+
+                    <View
+                    className="w-11/12 mt-4"
+                    >
+                        <Text
+                        className={styles.inputText}
+                        >
+                            Emergency Contact:
+                        </Text>
+                        <TextInput
+                        className={styles.inputField + " h-10"}
+                        placeholderTextColor="#0284BE"
+                        placeholder="Ex: (XXX) XXX - XXXX"
+                        onChangeText={setEmergencyContact}
+                        />
+                    </View>        
+
+                    <View
+                    className="w-full items-center absolute bottom-0"
+                    >
+                        <View
+                        className="flex-row"
+                        >
+                            <TouchableScale
+                            className="bg-red-500 w-10 h-10 m-6 pt-1 rounded-full items-center justify-center"
+                            activeScale={0.98}
+                            onPress={() => setScreenVisible(false)}
+                            >
+                                <Cancel 
+                                width={20}
+                                height={20}
+                                fill="#FFF"
+                                />
+                            </TouchableScale>
+
+                            <TouchableScale
+                            className="bg-green-500 w-10 h-10 m-6 pt-2 rounded-full items-center justify-center"
+                            activeScale={0.98}
+                            onPress={() => {
+                                setScreenVisible(false)
+                                if(changedName != "") setUserName(changedName);
+                            }}
+                            >
+                                <Check 
+                                width={45}
+                                height={45}
+                                fill="#FFF"
+                                />
+                            </TouchableScale>
+                        </View>
+
+                    </View>      
+
+                </SafeAreaView>
+            </Modal>
+            <SafeAreaView
+            className="flex-row w-full h-12 items-center"
+            >
+
+                <TouchableScale
+                className="flex-row w-1/2 h-12 mt-10 items-center"
+                activeScale={0.97}
+                onPress={() => setScreenVisible(true)}
+                >
+                    <Image 
+                    source={pfp}
+                    className="w-8 h-8 mr-4 ml-6"
+                    />
+                    <Text
+                    className="text-white "
+                    >
+                        {userName}
+                    </Text>
+                </TouchableScale>
+            </SafeAreaView>
+        </>
     );
 }
