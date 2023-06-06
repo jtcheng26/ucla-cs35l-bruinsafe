f405b96b9798be614af233260e2cf6138991fc42
Jeffrey Cheng <jtcheng226@gmail.com>
2023-06-05 19:37:41 -0700

    Improve map styling

--- components/screens/map/MapScreen.js
+++ components/screens/map/MapScreen.js
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 import { useState, useEffect } from "react";
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
 import NavBar from "../../overlays/NavBar";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import ProfileHeader from "../../overlays/ProfileHeader";
@@ -8,11 +8,11 @@ import WalkButton from "./walkButton";
 import WalkingPage from "./WalkingPage";
 import NumberReports from "./numberReports";
 import mapStyle from "./mapStyle.json";
-import moment from 'moment';
-import EscortList from './escortList';
-import axios from 'axios';
-import * as Location from 'expo-location';
-import {BASE_URL} from '../login/Login';
+import moment from "moment";
+import EscortList from "./escortList";
+import axios from "axios";
+import * as Location from "expo-location";
+import { BASE_URL } from "../login/Login";
 
 import MapViewDirections from "react-native-maps-directions";
 
@@ -32,102 +32,147 @@ const GOOGLE_MAPS_APIKEY = process.env.GOOGLE_APIKEY;
 
 //#020617
 export default function MapScreen() {
-    const [walking, setWalking] = useState(true);
-    const [location, setLocation] = useState({
-        latitude: 34.069201,
-        longitude: -118.443515,
-        latitudeDelta: 0.002,
-        longitudeDelta: 0.002
-    });
-    const [data, setData] = useState({
-        numReports: 0,
-        cityName: "Westwood Plaza",
-        stateName: "California"
-    });
-    const [currentDateTime, setDateTime] = useState(moment().format("hh:mm a"));
-    const getLocation = async () => {
-        try {
-            let curLocation = await Location.getCurrentPositionAsync({});
-            let locCopy = {...location};
-            locCopy.latitude = curLocation.coords.latitude;
-            locCopy.longitude = curLocation.coords.longitude;
-            setLocation(locCopy);
-        } catch (e) {
-            console.error(e);
-        }
+  const [walking, setWalking] = useState(true);
+  const [location, setLocation] = useState({
+    latitude: 34.069201,
+    longitude: -118.443515,
+    latitudeDelta: 0.002,
+    longitudeDelta: 0.002,
+  });
+  const [data, setData] = useState({
+    numReports: 0,
+    cityName: "Westwood Plaza",
+    stateName: "California",
+  });
+  const [path, setPath] = useState({})
+  const [currentDateTime, setDateTime] = useState(moment().format("hh:mm a"));
+  const getLocation = async () => {
+    try {
+      let curLocation = await Location.getCurrentPositionAsync({});
+      let locCopy = { ...location };
+      locCopy.latitude = curLocation.coords.latitude;
+      locCopy.longitude = curLocation.coords.longitude;
+      setLocation(locCopy);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  const fetchData = async () => {
+    try {
+      let dataCopy = { ...data };
+      let numReports = await axios.post(BASE_URL + "/report/search", {
+        latitude: location.latitude,
+        longitude: location.longitude,
+      });
+      let currentLocation = await axios.get(
+        "http://api.openweathermap.org/geo/1.0/reverse?lat=" +
+          location.latitude +
+          "&lon=" +
+          location.longitude +
+          "&cnt=1&APPID=1107f09a2cd574c391617612953ada00"
+      );
+      dataCopy.cityName = currentLocation.data[0].name;
+      dataCopy.stateName = currentLocation.data[0].state;
+      dataCopy.numReports = parseInt(10);
+      setData(dataCopy);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  useEffect(() => {
+    const interval = setInterval(fetchData, 1500);
+    return () => {
+      clearInterval(interval);
     };
-    const fetchData = async () => {
-        try {
-            let dataCopy = {...data};
-            let numReports = await axios.post(BASE_URL + "/report/search", {latitude: location.latitude, longitude: location.longitude});
-            let currentLocation = await axios.get("http://api.openweathermap.org/geo/1.0/reverse?lat=" + location.latitude + "&lon=" + location.longitude + "&cnt=1&APPID=1107f09a2cd574c391617612953ada00");
-            dataCopy.cityName = currentLocation.data[0].name;
-            dataCopy.stateName = currentLocation.data[0].state;
-            dataCopy.numReports = parseInt(10);
-            setData(dataCopy);
-        } catch (e) {
-            console.error(e)
-        }
+  }, []);
+  useEffect(() => {
+    const interval = setInterval(getLocation, 1500);
+    return () => {
+      clearInterval(interval);
     };
-    useEffect(() => {
-        const interval = setInterval(fetchData, 1500);
-        return () => {
-            clearInterval(interval);
-        }
-    }, [])
-    useEffect(() => {
-        const interval = setInterval(getLocation, 1500);
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
-    setInterval(() => {
-        setDateTime(moment().format("hh:mm a"))}, 2500);
-    return (
-        <View className="bg-sky-950 from-slate-950 flex-1 justify-center items-center h-full w-full">
-            <MapView 
-                provider={PROVIDER_GOOGLE}
-                customMapStyle={mapStyle}
-                className="w-full h-full py-18"
-                region={location}
-            />
-            <View className="absolute top-0 w-full">
-                <LinearGradient
-                    colors={['#020617', 'transparent']}
-                    start={{x : 0.5, y : 0.2}}
-                    end={{x: 0.5, y: 1}}
-                >
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                </LinearGradient>
-            </View>
-            <View className="absolute bottom-0 w-full">
-                <LinearGradient
-                    colors={['transparent', '#020617']}
-                    start={{x: 0.5, y: 0}}
-                    end={{x: 0.5, y: 0.8}}
-                >
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                    <Text> </Text>
-                </LinearGradient>
-            </View>
-            <ProfileHeader name={"David Smallberg"}/>
-            <NumberReports numReports={data.numReports}/>
-            {walking ? <WalkingPage locationName={data.cityName} walkerFullName={"Carey Nachenberg"} currentTime={currentDateTime}/> : <WalkButton text={"walk with someone"} onPress={setWalking} />}
-        </View>
-    )
+  }, []);
+  setInterval(() => {
+    setDateTime(moment().format("hh:mm a"));
+  }, 2500);
+  return (
+    <View className="flex-1 justify-center items-center h-full w-full">
+     
+      <MapView
+        // provider={PROVIDER_GOOGLE}
+        userInterfaceStyle="dark"
+        className="w-full h-full py-18"
+        region={location}
+        mapType="standard"
+        showsPointsOfInterest={false}
+        showsUserLocation
+        compassOffset={{
+            x: 0,
+            y: 50
+        }}
+        showsCompass
+      >
+        {GOOGLE_MAPS_APIKEY && walking ? (
+          <MapViewDirections
+            origin={origin}
+            destination={destination}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeColor="#FBBF24"
+            strokeWidth={6}
+            lineCap="round"
+            mode="WALKING"
+          />
+        ) : (
+          ""
+        )}
+              
+      </MapView>
+      <View className="absolute top-0 left-0 bottom-0 right-0 bg-blue-700/50" pointerEvents="none"  />
+
+      <View className="absolute top-0 w-full">
+        <LinearGradient
+          colors={["rgb(30 58 138)", "transparent"]}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.8 }}
+        >
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+        </LinearGradient>
+      </View>
+      <View className="absolute bottom-0 w-full">
+        <LinearGradient
+          colors={["transparent", "rgb(30 58 138)"]}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 1 }}
+        >
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+          <Text> </Text>
+        </LinearGradient>
+      </View>
+      <ProfileHeader name={"David Smallberg"} />
+      <NumberReports numReports={data.numReports} />
+      {walking ? (
+        <WalkingPage
+          locationName={data.cityName}
+          walkerFullName={"Carey Nachenberg"}
+          currentTime={currentDateTime}
+        />
+      ) : (
+        <WalkButton text={"walk with someone"} onPress={setWalking} />
+      )}
+    </View>
+  );
 }
