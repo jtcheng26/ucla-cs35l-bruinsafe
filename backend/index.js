7c4384bed479bc223d3c3e2b08d37dd488757dc0
Jeffrey Cheng <jtcheng226@gmail.com>
2023-06-06 21:47:27 -0700

    Socket functions and routes

--- backend/index.js
+++ backend/index.js
@@ -3,46 +3,34 @@ require("dotenv").config();
 const { mongoose, UserModel, ReportModel } = require("./models.js");
 
 const express = require("express");
-const ip = require('ip');
+const ip = require("ip");
 const ipAddress = ip.address();
 const app = express();
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const { onConnect } = require("./socket.js");
+io.on("connection", onConnect(io));
+
 const port = process.env.PORT || 8080;
 
-const { createHash } = require('crypto')
+const { createHash } = require("crypto");
 
 function hashpw(string) {
-  return createHash('sha256').update(string).digest('hex');
+  return createHash("sha256").update(string).digest("hex");
 }
 
 /* ======================= Walk Routes ======================= */
 
-const { walkRequest, walkAccept, getWalks } = require('./walk.js')
+const { walkRequest, walkAccept, getWalks } = require("./walk.js");
 
-io.on("connection", (socket) => {
-  
-  // socket.emit("connect", { message: "a new client connected" });
-  // socket.on("walker_id", (message) => {
-  //   console.log('From client: ', message)
-  //   io.emit("walker_id", message);
-  // });
+app.post("/walk/request", walkRequest);
 
-  // socket.on("walker_id", (message) => {
-  //   console.log("From server: ", message);
-  // });
+app.post("/walk/accept", walkAccept);
 
-  socket.on("disconnect", function () {
-    console.log("user disconnected");
-  });
-});
-
-app.post("/walk/request", walkRequest)
-
-app.post("/walk/accept", walkAccept)
-
-app.get("/walk/get", getWalks)
+app.get("/walk/get", getWalks);
 
 /* ======================= User Routes ======================= */
 
@@ -60,80 +48,76 @@ app.get("/user/:id", async (req, res) => {
 
 app.post("/user/login", async (req, res) => {
   const email = req.body.email;
-  const model = await UserModel.findOne({email: email}).exec();
+  const model = await UserModel.findOne({ email: email }).exec();
   if (!model) {
-    res.status(400).json({error : "User not found" });
+    res.status(400).json({ error: "User not found" });
   } else {
-    if (!req.body.password) res.send("Invalid password", 400)
-    const pw = hashpw(req.body.password)
+    if (!req.body.password) res.send("Invalid password", 400);
+    const pw = hashpw(req.body.password);
     if (model.password != pw) {
-      res.status(400).json({error : "Wrong password" });
+      res.status(400).json({ error: "Wrong password" });
     } else {
       res.status(200).json(model.toJSON());
     }
   }
-})
+});
 
 app.post("/user/create", (req, res) => {
   const email = req.body.email;
   const name = req.body.name;
-  if (!req.body.password) res.send("Invalid password", 400)
-  const pw = hashpw(req.body.password)
+  if (!req.body.password) res.send("Invalid password", 400);
+  const pw = hashpw(req.body.password);
 
-  UserModel.findOne( {$or: [{email: email}, { password: pw}] },
+  UserModel.findOne(
+    { $or: [{ email: email }, { password: pw }] },
     (err, exisitingUser) => {
-
       if (err) {
-        res.send("Internal Server Error", 500)
-      }
-      else if (existingUser)
-      {
-        if (existingUser.email == email){
-          res.status(400).json({ error: "User with specified email already exists" });
-        }
-        else
+        res.send("Internal Server Error", 500);
+      } else if (existingUser) {
+        if (existingUser.email == email) {
+          res
+            .status(400)
+            .json({ error: "User with specified email already exists" });
+        } else
           res.status(400).json({ error: "User with password already exists" });
-      }
-      else
-      {
+      } else {
         const model = new UserModel({ email: email, name: name, password: pw });
         model.save();
         res.status(200).json(model.toJSON());
       }
-    })
-
+    }
+  );
 });
 
 app.put("/user/edit", async (req, res) => {
   const id = req.body.id;
   const name = req.body.name;
-  const pw = hashpw(req.body.password)
+  const pw = hashpw(req.body.password);
 
   if (!mongoose.isValidObjectId(id)) {
     res.send("Invalid ID", 400);
     return;
   }
-  const model = await UserModel.findById(id)
+  const model = await UserModel.findById(id);
   if (!model) {
-    res.send("User not found", 404)
-  }
-  else if (model.password != pw) {
-    res.send("Wrong password", 400)
+    res.send("User not found", 404);
+  } else if (model.password != pw) {
+    res.send("Wrong password", 400);
   } else {
-    await UserModel.findByIdAndUpdate(id, { name: name })
-    res.send("Success", 200)
+    await UserModel.findByIdAndUpdate(id, { name: name });
+    res.send("Success", 200);
   }
-})
+});
 
 app.get("/users/nearby", async (req, res) => {
   const users = await UserModel.find({});
-  const jsonUsers = users.map(user => {
-    const json = user.toJSON()
-    delete json["password"]
-    return json
-  })
+  const jsonUsers = users.map((user) => {
+    const json = user.toJSON();
+    delete json["password"];
+    return json;
+  });
   res.send(jsonUsers);
-})
+});
 
 /* ======================= Report Routes ======================= */
 
@@ -143,8 +127,8 @@ const MAX_RADIUS = 0.1 * 0.1 + 0.1 * 0.1;
 app.post("/report/search", async (req, res) => {
   const myLoc = {
     latitude: req.body.latitude,
-    longitude: req.body.longitude
-  }
+    longitude: req.body.longitude,
+  };
   // optimize this for larger datasets in the future
   const all = await ReportModel.find({});
   const nearby = all.filter((report) => {
@@ -156,9 +140,20 @@ app.post("/report/search", async (req, res) => {
   res.send(all);
 });
 
-const REPORT_TYPES = ["Theft", "Assault", "Rape", "Abuse", "Kidnapping", 
-                    "Stalking", "Hate Crime", "Indecent Exposure", "Drug Distribution", 
-                    "Vandalism", "Solicitation", "Speeding"];
+const REPORT_TYPES = [
+  "Theft",
+  "Assault",
+  "Rape",
+  "Abuse",
+  "Kidnapping",
+  "Stalking",
+  "Hate Crime",
+  "Indecent Exposure",
+  "Drug Distribution",
+  "Vandalism",
+  "Solicitation",
+  "Speeding",
+];
 app.post("/report/create", (req, res) => {
   const types = req.body.types;
   const timestamp = new Date();
