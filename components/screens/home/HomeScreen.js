1cfa7fad561cdbb4536571fe2c848e28344b9829
Bach Ngo <ngo.b48@gmail.com>
2023-06-07 03:10:38 -0700

    added accept walking, addes comprehemsive styles for homepg

--- components/screens/home/HomeScreen.js
+++ components/screens/home/HomeScreen.js
@@ -8,13 +8,16 @@ import WalkingRequestPanel from './WalkingRequestPanel';
 import { useState, useEffect, useMemo } from 'react';
 import ReportsPanel from './ReportsPanel';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import useUserId from '../../hooks/useUserId';
+import MapScreen from '../map/MapScreen';
 
 
 
 export default function HomeScreen() {
-    const [users, setUsers] = useState([])
-    const [newUsers, setNewUsers] = useState([]);
+    const [walks, setWalks] = useState([])
     const [reports, setReports] = useState([]);
+    const {id} = useUserId();
+    const [walkAccepted, setWalkAccepted] = useState(false)
     const month = {
         "01": "Jan",
         "02": "Feb",
@@ -34,7 +37,8 @@ export default function HomeScreen() {
         const fetchNearbyUsers = async() => {
             try {
                 const response = await axios.get(BASE_URL + "/walk/get");
-                setUsers(response.data);
+                console.log(response.data)
+                setWalks(response.data.filter(u => u.user._id !== id));
             } catch(e) {
                 console.error(e)
             }
@@ -58,23 +62,46 @@ export default function HomeScreen() {
         fetchNearbyReports();
     }, [])
 
+    // useEffect(() => {
+    //     let newArr = walks.filter(u => u.state === 0);
+    //     setWalks(newArr)
+    // }, [walks])
+
     const handleDecline = (id) => {
-        let newArr = newUsers.filter(u => u._id !== id)
-        setUsers(newArr)
+        let newArr = walks.filter(u => u._id !== id)
+        setWalks(newArr)
     }
 
-    useEffect(() => {
-        if (!users) return [];
-        (async () => {
-            const cur_user_id = await AsyncStorage.getItem("@id");
-            let newArr = users.filter(u => u.user !== cur_user_id);
-            const fetched = await Promise.all(newArr.map(u => axios.get(BASE_URL + "/user/" + u.user)));
-            setNewUsers(fetched.map(f =>
-                f.data
-            ))
-        })()
-        
-    }, [users])
+    const handleAccept = (walk_id) => {
+        const connectWalk = async() => {
+            try {
+                const data = {
+                    user: id,
+                    id: walk_id
+                }
+
+                const response = await axios.post(BASE_URL + "/walk/accept", data);
+                console.log(response.data)
+            } catch(e) {
+                console.error(e)
+            }
+        }
+
+        connectWalk();
+        setWalkAccepted(true);
+        let newArr = walks.filter(u => u.state === 0);
+        setWalks(newArr)
+    }
+
+    // useEffect(() => {
+    //     let newArr = walks.filter(u => u.user._id !== id);
+    //     setWalks(newArr)
+    // }, [id, walks])
+    if(walkAccepted) {
+        return (
+            <MapScreen />
+        )
+    }
 
     return (
         <View className="flex-1 bg-sky-950">
@@ -88,15 +115,27 @@ export default function HomeScreen() {
             className="w-full h-2/5 items-center justify-center"
             >
                 <ScrollView
-                className="w-10/12"
+                className="w-11/12"
+                contentContainerStyle={{
+
+                }}
                 >
-                    {newUsers.map(user => (
+                    {(walks.length > 0) ? 
+                        (walks.map(walk => (
                         <WalkingRequestPanel 
-                        key={Math.random()}
-                        user={user}
+                        key={walk._id}
+                        user={walk.user}
+                        walk_id={walk._id}
                         onDecline={handleDecline}
+                        onAccept={handleAccept}
                         />
-                    ))}
+                        ))) : 
+                            (<Text
+                            className="text-sky-200 font-semibold top-full ml-4"
+                            >
+                                No Current Walking Requests
+                            </Text>)
+                    }
                 </ScrollView>
             </View>
             <Text
@@ -111,7 +150,8 @@ export default function HomeScreen() {
                 className="w-10/12"
                 horizontal
                 >
-                    {reports.map((report) => (
+                    {(reports.length > 0) ? 
+                        (reports.map((report) => (
                         <ReportsPanel
                         key={report._id}
                         type={report.types.join(", ")}
@@ -120,8 +160,15 @@ export default function HomeScreen() {
                             report.timestamp.slice(8,10) + 
                             ", " + report.timestamp.slice(0,4)
                         }
-                        />
-                    ))}
+                        />))) : 
+                        (
+                            <Text
+                            className="text-sky-200 font-semibold"
+                            >
+                                No Current Incidents Reported
+                            </Text>
+                        )
+                    }
                 </ScrollView>
             </View>
             
