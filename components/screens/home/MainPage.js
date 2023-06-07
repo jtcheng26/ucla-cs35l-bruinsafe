8543cb6eea768026e9897d56ceec3b0d793c5919
Jeffrey Cheng <jtcheng226@gmail.com>
2023-06-06 23:00:36 -0700

    Don't rerender map when switching pages

--- components/screens/home/MainPage.js
+++ components/screens/home/MainPage.js
@@ -1,25 +1,26 @@
-import { View } from "react-native"
+import { View } from "react-native";
 import { useState } from "react";
 import HomeScreen from "./HomeScreen";
-import MapScreen from "../map/MapScreen.js"
+import MapScreen from "../map/MapScreen.js";
 import ReportScreen from "./ReportScreen";
 import NavBar from "../../overlays/NavBar";
 import ProfileHeader from "../../overlays/ProfileHeader";
 
 export default function MainPage({ onLogout }) {
-    const [screen, setScreen] = useState("map");
-    let page = "";
-    const updateScreen = (newScreen) => {
-        setScreen(newScreen);
-    };
-    if (screen == "home") page = <HomeScreen />;
-    else if (screen == "map") page = <MapScreen />;
-    else if (screen == "report") page = <ReportScreen />;
-    return (
-        <View className="w-full h-full">
-            {page}
-            <ProfileHeader onLogout={onLogout}/>
-            <NavBar updateScreen={updateScreen} />
-        </View>
-    )
+  const [screen, setScreen] = useState("map");
+  let page = "";
+  const updateScreen = (newScreen) => {
+    setScreen(newScreen);
+  };
+  return (
+    <View className="w-full h-full">
+      <View className={screen === "map" ? "w-full h-full" : "hidden"}>
+        <MapScreen />
+      </View>
+      {screen === "home" && <HomeScreen />}
+      {screen === "report" && <ReportScreen />}
+      <ProfileHeader onLogout={onLogout} />
+      <NavBar updateScreen={updateScreen} />
+    </View>
+  );
 }
