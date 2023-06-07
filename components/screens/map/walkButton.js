1807c221ceb8ee1a9e0cf9789c9a5d1f33cd7a34
Pranav Puranam <pranav.puranam@gmail.com>
2023-06-07 02:21:16 -0700

    added walk accept functionality

--- components/screens/map/walkButton.js
+++ components/screens/map/walkButton.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import {BASE_URL} from '../../../constants';
 import useUserId from '../../hooks/useUserId';
 
-export default function walkButton({onPress, text, setMarker, setMarkerStyle, region, markerList, setMarkerList, regionCoords, walkPath, setWalkPath, curLocation}) {
+export default function walkButton({onPress, text, setMarker, setMarkerStyle, region, markerList, setMarkerList, regionCoords, walkPath, setWalkPath, curLocation, setWaiting}) {
     const [buttonText, setButtonText] = useState(text);
     const { id } = useUserId()
     // useEffect(() => {
@@ -37,6 +37,7 @@ export default function walkButton({onPress, text, setMarker, setMarkerStyle, re
             let copyPath = {...walkPath};
             copyPath.start = regionCoords;
             setWalkPath(copyPath);
+            setWaiting(true);
             // copyMarkerList.push(<Marker coordinate={regionCoords} pinColor="#FBBF24"/>);
             // setMarkerList(copyMarkerList);
         } else if (buttonText === "set end") {
@@ -56,6 +57,7 @@ export default function walkButton({onPress, text, setMarker, setMarkerStyle, re
             //const pushCoords = await axios.post(BASE_URL + "/walk/request", {origin: copyPath.start, destination: copyPath.end, user: id });
             //console.log(pushCoords.data)
             // setMarkerList([{...copyPath, user: id}]);
+            setWaiting(true);
             onPress(2);
             // console.log(pushCoords)
             // copyMarkerList.push(<Marker coordinate={regionCoords} pinColor="#BA132C"/>);
