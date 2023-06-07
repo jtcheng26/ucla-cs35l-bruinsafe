9acc0ad902883fb3b54d48ca461df6f99ab2bcd4
Jeffrey Cheng <jtcheng226@gmail.com>
2023-06-07 05:24:34 -0700

    Searching state and ending walk functionality

--- components/screens/map/confirmPath.js
+++ components/screens/map/confirmPath.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useEffect } from 'react';
 
-export default function confirmPath({onPress, coordinates, setMarkerList, setThePath, copyPath, setWalking, setWaiting, waiting, setCurrentWalker}) {
+export default function confirmPath({onPress, coordinates, setMarkerList, setThePath, copyPath, setWalking, setWaiting, waiting, setCurrentWalker, setCurrentWalkId}) {
     const { id } = useUserId(); 
     const [confirmed, setConfirmed] = useState(false);
 
@@ -28,6 +28,7 @@ export default function confirmPath({onPress, coordinates, setMarkerList, setThe
                 let allWalks = (await axios.get(BASE_URL + "/walk/get")).data; //array of all WalkModels in DB
                 let result = allWalks.filter(walk => (walk.user._id == id && walk.state == 1)); //filter for the WalkModel that pertains to User and has been accepted
                 if (result.length > 0) {
+                    setCurrentWalkId(result._id)
                     setCurrentWalker(result[0].guardian.name) //if there is such a walkmodel, setCurrentWalker to guardian of Walk
                     setWaiting(false);
                     onPress(1);
@@ -41,9 +42,10 @@ export default function confirmPath({onPress, coordinates, setMarkerList, setThe
 
     const handleClick = async (confirm) => {
         if (confirm) {
-            setWalking(true);
+            setWaiting(true);
             const pushCoords = await axios.post(BASE_URL + "/walk/request", {origin: copyPath.start, destination: copyPath.end, user: id }); //Create walk request
             setConfirmed(true);
+            onPress(3)
         } else {
             onPress(0);
             setMarkerList([]);
