c766cba3b891498ebeedb1456c66dd860b1d1893
Bach Ngo <ngo.b48@gmail.com>
2023-06-08 15:22:41 -0700

    put hooks in useeffect

--- components/hooks/useUserId.js
+++ components/hooks/useUserId.js
@@ -1,10 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function useUserId() {
     const [id, setId] = useState("")
-    AsyncStorage.getItem("@id", (err, res) => {
-        if (!err) setId(res)
-    })
+    useEffect(() => {
+        AsyncStorage.getItem("@id", (err, res) => {
+            if (!err) setId(res)
+        })
+    }, [])
+    
     return { id }
 }
