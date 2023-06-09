21c071665ffa3f499fcec1245aa074f8267c0ded
Pranav Puranam <pranav.puranam@gmail.com>
2023-06-08 18:03:58 -0700

    End walk on both phones, implement progress bar functionality

--- components/screens/map/ProgessBar.js
+++ components/screens/map/ProgessBar.js
@@ -1,24 +1,36 @@
-import { View, Text, Image } from 'react-native';
-import { useState } from 'react';
+import { View, Text, Image, Animated } from 'react-native';
+import { useEffect, useState , useRef} from 'react';
 import TouchableScale from 'react-native-touchable-scale';
 
 
-export default function ProgressBar() {
-    const [progress, setProgress] = useState(0.9); //potential issue: setProgress needs to update progress for bar to move. Component should take in current location, and destination and check for distance.
-    let progressStyle = "absolute ml-3 w-full py-1 rounded-full bg-amber-400/100 z-0 ";
+export default function ProgressBar({ progress=0 }) {
+    // const [progress, setProgress] = useState(0.9); //potential issue: setProgress needs to update progress for bar to move. Component should take in current location, and destination and check for distance.
+    let progressStyle = "absolute ml-3 py-1 rounded-full bg-amber-400/100 z-20 transition-all duration-500 ease-in";
     const padArray = new Array(2,3,4,5,6,7,8,9,10,11,12,14,16,20,24);
     var newStyle = 0;
-    for (let i = 0; i < padArray.length; i++) {
-        if (Math.abs(padArray[i] - (24 * progress)) < Math.abs(newStyle - (24 * progress))) {
-            newStyle = padArray[i];
+    const prog = useRef(new Animated.Value(0)).current
+    useEffect(() => {
+        if (progress || progress === 0)  {
+            const t = Animated.timing(prog, {
+                toValue: progress * 200,
+                duration: 500,
+                useNativeDriver: false,
+            });
+            t.start()
+            return () => t.stop()
         }
-    }
-    progressStyle += "px-" + String(newStyle);
+    }, [progress, prog])
+    // for (let i = 0; i < padArray.length; i++) {
+    //     if (Math.abs(padArray[i] - (24 * progress)) < Math.abs(newStyle - (24 * progress))) {
+    //         newStyle = padArray[i];
+    //     }
+    // }
+    // progressStyle += "px-" + String(newStyle);
     return (
         <View className="relative mb-2">
-            <TouchableScale className="absolute ml-3 w-full px-24 py-1 rounded-full bg-amber-500/40 z-0" activeScale={1}>
+            <TouchableScale className="absolute ml-3 py-1 rounded-full bg-amber-500/40 z-0 w-full" style={{width: 200}} activeScale={1}>
             </TouchableScale>
-            <TouchableScale className={progressStyle} activeScale={1}>
+            <TouchableScale className={progressStyle} style={{width: prog ? prog : 1}} activeScale={1}>
             </TouchableScale>
         </View>
     );
--- components/utils/timeToDestination.js
+++ components/utils/timeToDestination.js
@@ -1,5 +1,25 @@
 import { dist } from "./isOutsidePath";
 
+export function progressToDestination(currentLocation, pathArray) {
+    if (pathArray) {
+        const totalDistance = pathArray.distance;
+        const totalDuration = Math.floor(pathArray.duration);
+        const path = pathArray.legs[0].steps;
+        let curMin = path[0];
+        let dirSum = 0.0;
+        let curDirSum = 0.0;
+        path.forEach(element => {
+            if (dist(currentLocation, {latitude: ((element.start_location.lat + element.end_location.lat) / 2), longitude: ((element.start_location.lng + element.end_location.lng) / 2)}) < dist(currentLocation, {latitude: ((curMin.start_location.lon + curMin.end_location.lon) / 2), longitude: ((curMin.start_location.lng + curMin.end_location.lng) / 2)})) {
+                curMin = element;
+                dirSum = curDirSum;
+            }
+            curDirSum += element.duration.value / 60;
+        });
+        return ((dirSum) / totalDistance);
+    }
+    return 0
+}
+
 export default function timeToDestination(currentLocation, pathArray) {
     if (pathArray) {
         const totalDistance = pathArray.distance;
